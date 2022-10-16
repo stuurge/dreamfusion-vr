6285f994325c1affa4ee48ab61c365fcaf14eaaa
Alton Sturgis <asturgis@berkeley.edu>
2022-10-16 04:41:54 -0700

    wake up new package

---
+++ dreamfusion-vr/Assets/Scripts/FileDownloader.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FileDownloader : MonoBehaviour
+{
+    public string fileID;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+}
